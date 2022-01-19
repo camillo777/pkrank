454de8f580c01a618bc47be9b8cd4bc2f78809d4
Camillo Ferraris <camillo777@gmail.com>
2022-01-19 09:57:46 +0100

    aa

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(error, req, res, next) {
+  console.log( req.baseUrl );
+  next;
+});
+
 app.use( `/${ process.env.PATH }`, indexRouter);
 //app.use('/pkrank/users', usersRouter);
 
