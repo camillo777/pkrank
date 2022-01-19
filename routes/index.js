8312fdd1b0b86c1b30e7375eb2c758207a65a6cf
Camillo Ferraris <camillo777@gmail.com>
2022-01-19 10:07:12 +0100

    a

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,9 @@ async function main() {
 router.get( `/${ process.env.PATH }`, function(req, res, next) {
   res.render('index', { title: 'PK RANK' });
 });
+router.get( `/pkrank`, function(req, res, next) {
+  res.render('index', { title: 'PK RANK' });
+});
 
 router.post( `/${ process.env.PATH }`, function(req,res) {
   console.log(req.body);
