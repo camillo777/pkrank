b3a8ef9ce1eda1576716de12b36728abb138ccc6
Camillo Ferraris <camillo777@gmail.com>
2022-01-19 10:03:59 +0100

    a

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(error, req, res, next) {
-  console.log( req.baseUrl );
+app.use((req, res, next) => {
+  console.log( 'TEST', req.baseUrl );
   next();
 });
 
