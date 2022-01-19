662290641bc6e3969cab5416cb36550d9b67c3aa
Camillo Ferraris <camillo777@gmail.com>
2022-01-19 10:04:30 +0100

    a

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use( `/pkrank`, indexRouter);
+app.use( `/`, indexRouter);
 //app.use('/pkrank/users', usersRouter);
 
 // catch 404 and forward to error handler
