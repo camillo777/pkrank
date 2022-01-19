bd91c72beba91ee20c8913799a662c2d380dca6b
Camillo Ferraris <camillo777@gmail.com>
2022-01-19 10:47:25 +0100

    css cdn

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('/pkrank',path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   console.log( 'req.baseUrl', req.baseUrl );
